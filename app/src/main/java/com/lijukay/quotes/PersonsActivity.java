018574baa1957a297c185dccfb03f64c0c58a7d5
Lijukay <lico.keins@gmail.com>
2022-07-27 00:32:31 +0200

    I added the possibility to open a new Intent by clicking on Unknown. Clicking on William Shakespeare will show a toast message

--- app/src/main/java/com/lijukay/quotes/PersonsActivity.java
+++ app/src/main/java/com/lijukay/quotes/PersonsActivity.java
@@ -1,30 +1,31 @@
 package com.lijukay.quotes;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.net.wifi.hotspot2.pps.HomeSp;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.util.SeslRoundedCorner;
+import androidx.appcompat.util.SeslSubheaderRoundedCorner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import dev.oneuiproject.oneui.layout.ToolbarLayout;
 
 public class PersonsActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.persons_activity);
-
-        ToolbarLayout toolbarLayoutPersons = findViewById(R.id.toolbar_persons);
-
-        String person = "Person's name not set";
-
-        Bundle extras = getIntent().getExtras();
-        if(extras != null){
-            person = extras.getString("person");
-        }
-
-        toolbarLayoutPersons.setTitle(person);
-
     }
-
-
-
 }
