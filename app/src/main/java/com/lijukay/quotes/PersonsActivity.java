41c84dcff091a7bc25538ba28bfff3c2df03fb62
Lijukay <lico.keins@gmail.com>
2022-07-27 17:14:25 +0200

    Changing in persons_activity.xml and PersonsActivity.java
    I set the Navigation Icon of persons_activity.xml as back Navigation
    
    I changed the text in persons_activity.xml to a plan I have
    
    I inserted the code BlackMesa123 wrote me in my Issue about the wrong layout. Thanks to you, BlackMesa123

--- app/src/main/java/com/lijukay/quotes/PersonsActivity.java
+++ app/src/main/java/com/lijukay/quotes/PersonsActivity.java
@@ -1,23 +1,10 @@
 package com.lijukay.quotes;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
-import android.net.wifi.hotspot2.pps.HomeSp;
+
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
+import android.widget.FrameLayout;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.util.SeslRoundedCorner;
-import androidx.appcompat.util.SeslSubheaderRoundedCorner;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 
 import dev.oneuiproject.oneui.layout.ToolbarLayout;
 
@@ -27,5 +14,15 @@ public class PersonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.persons_activity);
+
+        ToolbarLayout toolbarLayout = findViewById(R.id.toolbar_persons);
+        toolbarLayout.setNavigationButtonAsBack();
+        toolbarLayout.getAppBarLayout().addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
+            final int totalScrollRange = appBarLayout.getTotalScrollRange();
+            FrameLayout content = findViewById(dev.oneuiproject.oneui.R.id.main_content);
+            if (content != null) {
+                content.setTranslationY(((float) (Math.abs(verticalOffset) - totalScrollRange)) / 2.0f);
+            }
+        });
     }
 }
